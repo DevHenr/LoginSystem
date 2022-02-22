850527025d6c9712ae2bd65e931547547c55a4c0
Thiago Santos <61206196+DevHenr@users.noreply.github.com>
2022-02-21 23:41:06 -0300

    fifth commit

--- CLS_Full/Home.php
+++ CLS_Full/Home.php
@@ -2,7 +2,6 @@
 include("auth_session.php");
 ?>
 
-
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -11,68 +10,49 @@ include("auth_session.php");
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-
-
-
-
-  <script type="text/javascript"
-  src="Confirm.js"></script>
-  
-  
-
-
+<script type="text/javascript" src="Confirm.js"></script>
 
 <title>Etec Albert Einstein</title>
 <link rel="stylesheet" href="Test.css">
 </head>
 
-
 <body class="Home">
 
-    <header>
-        <div class="navbar">
-		
-		
-		
-  <a class="active" href="Home.php"><i class="fa fa-fw fa-home"></i>Home</a> 
-     <a href="Sobre.php"><i class="fa fa-info-circle"></i>Sobre</a>
+<header>
+  <div class="navbar">
+
+    <a class="active" href="Home.php"><i class="fa fa-fw fa-home"></i>Home</a> 
+    <a href="Sobre.php"><i class="fa fa-info-circle"></i>Sobre</a>
+
   <div class="dropdown">
+
     <button class="dropbtn">TCC  
-      <i class="fa fa-caret-down"></i>
+        <i class="fa fa-caret-down"></i>
     </button>
-    <div class="dropdown-content">
-      <a href="Tema.php">Tema TCC</a>
-	
-      <a href="Membros.php">Membros</a>
-	  
-    </div>
+
+  <div class="dropdown-content">
+
+    <a href="Tema.php">Tema TCC</a>
+    <a href="Membros.php">Membros</a>
+
+  </div>
   </div> 
-   <a href="Contatos.php"><i class="fa fa-fw fa-envelope"></i>Contatos</a>
-   <a href="Logout.php" id="logout">Sair</a>
-   
+    <a href="Contatos.php"><i class="fa fa-fw fa-envelope"></i>Contatos</a>
+    <a href="Logout.php" id="logout">Sair</a>
+  </div>
 
-   
-   
+  <div class="CLS">
+    <h1 class="site-title">Conversor de Língua de Sinais </h1>
+  </div>
 
-</div>
-<div class="CLS">
-        <h1 class="site-title">Conversor de Língua de Sinais </h1>
-		</div>
-        <h2 class="site-subtitle">-- CLS --</h2>
-      </header>
-	  
-	  
-	  
-	 
-	  <img src="LOGO.png" alt="Logo CLS" class="Logo">
-	 
-	 
-	 
-	 
-	 <div class="Logout">
-        <p class="Log">Bem Vindo, <?php echo $_SESSION['username']; ?>!</p>
-        </div>
-	 
-	 
+    <h2 class="site-subtitle">-- CLS --</h2>
+</header>
+
+    <img src="LOGO.png" alt="Logo CLS" class="Logo">
+
+<div class="Logout">
+    <p class="Log">Bem Vindo, <?php echo $_SESSION['username']; ?>!</p>
+</div> 
+
 </body>
 <html>
